0c5573829c3062387ad94976c9cf3563c0fd0cbf
dgdeivis <dgdeivis116@gmail.com>
2021-03-30 10:45:46 +0300

    Intro

---
+++ js/01-comments.js
@@ -0,0 +1,7 @@
+// singel line comment
+
+/*
+ multi
+ line 
+ comment
+*/
---
+++ js/03-veiksmai.js
@@ -0,0 +1,10 @@
+const nr1 = 8;
+const nr2 = 5;
+const sum = nr1 + nr2;
+console.log(sum)
+const minus = nr1 - nr2;
+const multi = nr1 * nr2;
+const divide = nr1 / nr2;
+
+const combo = (nr1 + nr2) * (5 / nr2);
+console.log(combo);
