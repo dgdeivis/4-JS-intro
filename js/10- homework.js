be59745dfdcb5016a939822508a81e301ca9081c
dgdeivis <dgdeivis116@gmail.com>
2021-04-01 11:07:11 +0300

    JS

---
+++ js/10- homework.js
@@ -0,0 +1,27 @@
+const akys = 'raudonos';
+
+if (akys === 'melynos') {
+    console.log('Oj koks/kokia fainulka');
+} else if (akys === 'zalios') {
+    console.log('Oj koks/kokia protingi?...');
+} else if (akys === 'rudos') {
+    console.log('Fu fu fu... melagis...');
+} else if (akys === 'raudonos') {
+    console.log('programeris 🥳🥳🥳');
+} else {
+    console.log('Sorry, negaliu pagirti, nes nezinau tokios spalvos...');
+}
+
+const masina = 'juoda';
+
+if (masina === 'balta') {
+    console.log('negera');
+} else if (masina === 'melyna') {
+    console.log('gera') ;
+} else if (masina === 'geltona') {
+console.log('reta') ;
+} else if (masina === 'juoda') {
+console.log('labai gera');
+} else {
+console.log('tokios masinos spalvos nezinau');
+}
